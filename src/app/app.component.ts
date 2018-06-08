340370d25e20a07d32ec958b03de7e498afcbe66
marklavelle <marklavelle1@gmail.com>
2018-06-08 13:09:31 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+  constructor(
+  private afs: AngularFirestore,
+  private db: AngularFireDatabase,
+  private afAuth: AngularFireAuth
+  ) {}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyBASNZZN_j7Zp6qeWGs0dDv0nEperxu-c0",
+    authDomain: "fire-trip-1-prod.firebaseapp.com",
+    databaseURL: "https://fire-trip-1-prod.firebaseio.com",
+    projectId: "fire-trip-1-prod",
+    storageBucket: "",
+    messagingSenderId: "759943728491"
+  }
+};
